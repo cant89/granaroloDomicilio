bf9700270f7156342b1441fe958304a0ab7f82c3
Davide Cantelli <davide.cantelli@takeaway.com>
2020-03-30 01:30:20 +0200

    tel + more

--- src/components/lisItem.js
+++ src/components/lisItem.js
@@ -1,74 +1,84 @@
-import { useState } from 'preact/hooks';
+import { useState } from "preact/hooks";
 
 export const ListItem = ({ name, tel, site, mail, note }) => {
-	const [infoVisible, setInfoVisible] = useState(false);
-	const encodedName = encodeURIComponent(name);
-	const encodedCity = encodeURIComponent(process.env.PREACT_APP_CITY);
-	const searchUrl = `https://www.google.com/search?q=${encodedName}%20${encodedCity}`;
+   const [infoVisible, setInfoVisible] = useState(false);
+   const encodedName = encodeURIComponent(name);
+   const encodedCity = encodeURIComponent(process.env.PREACT_APP_CITY);
+   const searchUrl = `https://www.google.com/search?q=${encodedName}%20${encodedCity}`;
 
-	function handleClick() {
-		setInfoVisible(!infoVisible);
-	}
+   function handleClick() {
+      setInfoVisible(!infoVisible);
+   }
 
-	return (
-		<div class="rounded-lg border bg-gray-200 p-4 md:p-5 my-5 text-md lg:text-xl font-semibold text-gray-700">
-			<div class="flex justify-between items-center">
-				<span>
-					<a class="hover:underline" href={searchUrl} target="_blank" rel="noopener noreferrer">{name}</a>
-				</span>
-				<div class="flex">
-					{note && (
-						<span
-							onClick={handleClick}
-							class="inline-block mx-1 md:mx-2 w-8 h-8 cursor-pointer text-center leading-8 bg-yellow-300 rounded-lg"
-							role="img"
-							aria-label="warning"
-						>
-							⚠️
-						</span>
-					)}
-					{site && (
-						<a href={`${site}`}>
-							<span
-								onClick={handleClick}
-								class="inline-block mx-1 md:mx-2 w-8 h-8 cursor-pointer text-center leading-8 bg-orange-300 rounded-lg"
-								role="img"
-								aria-label="website"
-							>
-							🌐
-							</span>
-						</a>
-					)}
-					{mail && (
-						<a href={`mailto:${mail}`}>
-							<span
-								onClick={handleClick}
-								class="inline-block mx-1 md:mx-2 w-8 h-8 cursor-pointer text-center leading-8 bg-blue-300 rounded-lg"
-								role="img"
-								aria-label="e-mail"
-							>
-							✉️
-							</span>
-						</a>
-					)}
-					{tel && (
-						<a href={`tel:${tel}`}>
-							<span
-								class="inline-block mx-1 md:mx-2 w-8 h-8 bg-green-300 text-center leading-8 rounded-lg cursor-pointer"
-								role="img"
-								aria-label="telephone"
-							>
-							📞
-							</span>
-						</a>
-					)}
-				</div>
-			</div>
-			{infoVisible && (
-				<div class="block mt-10">
-					<p class="text-yellow-700 text-sm md:text-md lg:text-lg">{note}</p>
-				</div>
-			)}
-		</div>
-	);
+   return (
+      <div class="rounded-lg border bg-gray-200 p-4 md:p-5 my-5 text-md lg:text-xl font-semibold text-gray-700">
+         <div class="flex justify-between items-center">
+            <span>
+               <a
+                  class="hover:underline"
+                  href={searchUrl}
+                  target="_blank"
+                  rel="noopener noreferrer"
+               >
+                  {name}
+               </a>
+            </span>
+            <div class="flex">
+               {note && (
+                  <span
+                     onClick={handleClick}
+                     class="inline-block mx-1 md:mx-2 w-8 h-8 cursor-pointer text-center leading-8 bg-yellow-300 rounded-lg"
+                     role="img"
+                     aria-label="warning"
+                  >
+                     ⚠️
+                  </span>
+               )}
+               {site && (
+                  <a href={`${site}`}>
+                     <span
+                        onClick={handleClick}
+                        class="inline-block mx-1 md:mx-2 w-8 h-8 cursor-pointer text-center leading-8 bg-orange-300 rounded-lg"
+                        role="img"
+                        aria-label="website"
+                     >
+                        🌐
+                     </span>
+                  </a>
+               )}
+               {mail && (
+                  <a href={`mailto:${mail}`}>
+                     <span
+                        onClick={handleClick}
+                        class="inline-block mx-1 md:mx-2 w-8 h-8 cursor-pointer text-center leading-8 bg-blue-300 rounded-lg"
+                        role="img"
+                        aria-label="e-mail"
+                     >
+                        ✉️
+                     </span>
+                  </a>
+               )}
+               {tel &&
+                  tel.map(num => (
+                     <a href={`tel:${num.replace(/\s/g, "")}`}>
+                        <span
+                           class="inline-block mx-1 md:mx-2 w-8 h-8 bg-green-300 text-center leading-8 rounded-lg cursor-pointer"
+                           role="img"
+                           aria-label="telephone"
+                        >
+                           📞
+                        </span>
+                     </a>
+                  ))}
+            </div>
+         </div>
+         {infoVisible && (
+            <div class="block mt-10">
+               <p class="text-yellow-700 text-sm md:text-md lg:text-lg">
+                  {note}
+               </p>
+            </div>
+         )}
+      </div>
+   );
 };
--- src/index.js
+++ src/index.js
@@ -1,65 +1,75 @@
-import { h, Component } from 'preact';
-import { Router } from 'preact-router';
-import { Link } from 'preact-router/match';
+import { h, Component } from "preact";
+import { Router } from "preact-router";
+import { Link } from "preact-router/match";
 
-import 'tailwindcss/dist/tailwind.min.css';
+import "tailwindcss/dist/tailwind.min.css";
 
 // Code-splitting is automated for routes
-import Home from './routes/home.js';
-import Form from './routes/form.js';
+import Home from "./routes/home.js";
+import Form from "./routes/form.js";
 
 // Constants
 const SEARCH = process.env.PREACT_APP_DATA_SOURCE;
 
 export default class App extends Component {
+   state = {
+      results: {},
+      isHomepage: true
+   };
 
-	state = {
-		results: {},
-		isHomepage: true,
-	}
-	
-	handleRoute = e => {
-		this.currentUrl = e.url;
-		this.setState({isHomepage: e.url === "/"});
-	};
+   handleRoute = e => {
+      this.currentUrl = e.url;
+      this.setState({ isHomepage: e.url === "/" });
+   };
 
-	componentDidMount() {
-		fetch(
-			`${SEARCH}?q=${Math.random()
-				.toString(36)
-				.split('.')}`
-		)
-			.then(r => r.json())
-			.then(json => {
-				this.setState({
-					results: json,
-					resultBkp: json
-				});
-			});
-	}
+   componentDidMount() {
+      fetch(
+         `${SEARCH}?q=${Math.random()
+            .toString(36)
+            .split(".")}`
+      )
+         .then(r => r.json())
+         .then(json => {
+            this.setState({
+               results: json,
+               resultBkp: json
+            });
+         });
+   }
 
-	render(props, { isHomepage, results }) {
-		return (
-			<div id="app" class="px-5 max-w-screen-md mx-auto">
-				<nav class="flex justify-center md:justify-end items-center">
-					{
-						isHomepage
-							? null
-							: <Link class="m-5 text-blue-500 hover:text-blue-800" href="/">Ritorna alla ricerca</Link>
-					}
-					<Link class="m-5 bg-blue-500 inline-block hover:bg-blue-700 text-white font-bold px-2 py-1 rounded" href="/form">Aggiungi un'attività</Link>
-				</nav>
-				<h1 class="font-sans text-4xl md:text-5xl lg:text-6xl pt-10 text-gray-800 text-center capitalize">
-					<span class="block sm:inline-block" role="img" aria-label="biker">
-						🚴
-					</span>
-					{`${process.env.PREACT_APP_CITY} a Domicilio`}
-				</h1>
-				<Router onChange={this.handleRoute}>
-					<Home path="/" results={results} />
-					<Form path="/form" />
-				</Router>
-			</div>
-		);
-	}
+   render(props, { isHomepage, results }) {
+      return (
+         <div id="app" class="px-5 max-w-screen-md mx-auto">
+            <nav class="flex justify-center md:justify-end items-center">
+               {isHomepage ? null : (
+                  <Link class="m-5 text-blue-500 hover:text-blue-800" href="/">
+                     Ritorna alla ricerca
+                  </Link>
+               )}
+               {isHomepage ? (
+                  <Link
+                     class="m-5 bg-blue-500 inline-block hover:bg-blue-700 text-white font-bold px-2 py-1 rounded"
+                     href="/form"
+                  >
+                     Aggiungi un'attività
+                  </Link>
+               ) : null}
+            </nav>
+            <h1 class="font-sans text-4xl md:text-5xl lg:text-6xl pt-10 text-gray-800 text-center capitalize">
+               <span
+                  class="block sm:inline-block"
+                  role="img"
+                  aria-label="biker"
+               >
+                  🚴
+               </span>
+               {`${process.env.PREACT_APP_CITY} a Domicilio`}
+            </h1>
+            <Router onChange={this.handleRoute}>
+               <Home path="/" results={results} />
+               <Form path="/form" />
+            </Router>
+         </div>
+      );
+   }
 }
